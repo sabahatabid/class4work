066d055922c7da37fabb7e67150ba4d85e31c0e0
sabahatabid <161682926+sabahatabid@users.noreply.github.com>
2024-10-04 19:58:55 +0100

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,25 +1,55 @@
-import Link from "next/link";
+function CountryName({ params }: { params: { names: string } }) {
+  const country_data = [
+    {
+      name: "pakistan",
+      population: "251269164",
+      capital: "Islamabad",
+      important_cities: ["Karachi", "Lahore", "Sialkot"],
+    },
+    {
+      name: "china",
+      population: "1419321278",
+      capital: "Beijing",
+      important_cities: ["Shanghai", "Beijing", "Guangzhou"],
+    },
+    {
+      name: "iran",
+      population: "91780985",
+      capital: "Tehran",
+      important_cities: ["Isfahan", "Tabriz", "Qom"],
+    },
+    {
+      name: "turkey",
+      population: "87527081",
+      capital: "Ankara",
+      important_cities: ["Istanbul", "Bursa", "Izmir"],
+    },
+    {
+      name: "india",
+      population: "1450935791",
+      capital: "New Delhi",
+      important_cities: ["Mumbai", "Kolkata", "Chennai"],
+    },
+  ];
 
-export default function CountryList() {
+ 
 
-  const countries = ["pakistan", "china", "iran", "turkey", "india"];
+  const selectedCountry = country_data.find(
+    (country) => country.name === params.names
+  );
 
-    return (
-        <div style={{backgroundColor:"lightgrey"}}>
-            <h1>Country List</h1>
-            <div >
-        <ol>
-          {countries.map((country, index) => (
-            <li
-              key={index}>
-              <Link
-                href={`/country/${country}`}>
-                {country}
-              </Link>
-            </li>
-          ))}
-        </ol>
-      </div>
-            </div>
-    );
+  if (!selectedCountry) {
+    return <div>Country not found!</div>;
+  }
+
+  return (
+    <div style={{backgroundColor:"lightseagreen"}}>
+      <h1>Country Name : {selectedCountry.name}</h1>
+      <h3>Population: {selectedCountry.population}</h3>
+      <h4>Capital: {selectedCountry.capital}</h4>
+      <h5>Important Cities: {selectedCountry.important_cities.join(", ")}</h5>
+    </div>
+  );
 }
+
+export default CountryName;
